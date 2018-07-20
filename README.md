9a6541982c2e1ffdf9515feb5920a9484007f1f7
wmzy <1256573276@qq.com>
2018-07-20 17:26:08 +0800

    a rough implementation

---
+++ README.md
@@ -0,0 +1,13 @@
+[![Build Status](https://travis-ci.org/wmzy/gitlab-as-npm-registry.svg?branch=master)](https://travis-ci.org/wmzy/gitlab-as-npm-registry)
+[![Coverage Status](https://coveralls.io/repos/github/wmzy/gitlab-as-npm-registry/badge.svg?branch=master)](https://coveralls.io/github/wmzy/gitlab-as-npm-registry?branch=master)
+# Gitlab as npm registry
+
+> Gitlab as npm registry.
+
+## Install
+
+```bash
+npm install gitlab-as-npm-registry
+```
+
+## Usage
---
+++ gitlab-client.js
@@ -0,0 +1,22 @@
+const wretch = require('node-wretch');
+
+module.exports = class Gitlab {
+  constructor(baseURL, token) {
+    this.baseURL = baseURL;
+    this.token = token;
+    this.client = wretch(`${baseURL}/api/v4`).headers({'PRIVATE-TOKEN': token})
+  }
+
+  // https://docs.gitlab.com/ee/api/tags.html#list-project-repository-tags
+  projectTags(projectId) {
+    return this.client.url(`/projects/${encodeURIComponent(projectId)}/repository/tags`).get().json()
+  }
+
+  // https://docs.gitlab.com/ee/api/jobs.html#download-the-artifacts-archive
+  downloadArtifacts(projectId, refName, job) {
+    return this.client.url(`/projects/${encodeURIComponent(projectId)}/jobs/artifacts/${refName}/download?job=${job}`).get().res(res => {
+      console.log('res', res)
+      return res.body
+    })
+  }
+}
